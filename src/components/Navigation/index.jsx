390f9803ad2ea6b538118fe2fe67170cb6ff231a
Jana Malecka <jm.jmalecka@gmail.com>
2021-05-30 15:02:03 +0200

    hamburger menu on the right side

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -7,46 +7,48 @@ const Navigation = () => {
 
   return (
     <>
-      <button
-        onClick={() =>
-          menuOpen === true ? setMenuOpen(false) : setMenuOpen(true)
-        }
-        className={menuOpen ? 'hamburger hamburger--open' : 'hamburger'}
-        aria-label="menu"
-      >
-        <span></span>
-        <span></span>
-        <span></span>
-      </button>
-      {menuOpen === true ? (
-        <ul>
-          <li>
-            <a href="#home" className="nav-link">
-              Domů
-            </a>
-          </li>
-          <li>
-            <a href="#explore" className="nav-link">
-              Poznejte své město
-            </a>
-          </li>
-          <li>
-            <a href="#whyplay" className="nav-link">
-              Proč hrát naše hry
-            </a>
-          </li>
-          <li>
-            <a href="#buy" className="nav-link">
-              Koupit hru
-            </a>
-          </li>
-          <li>
-            <a href="#contact" className="nav-link">
-              Kontakt
-            </a>
-          </li>
-        </ul>
-      ) : null}
+      <div className="navigation">
+        <button
+          onClick={() =>
+            menuOpen === true ? setMenuOpen(false) : setMenuOpen(true)
+          }
+          className={menuOpen ? 'hamburger hamburger--open' : 'hamburger'}
+          aria-label="menu"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        {menuOpen === true ? (
+          <ul>
+            <li>
+              <a href="#home" className="nav-link">
+                Domů
+              </a>
+            </li>
+            <li>
+              <a href="#explore" className="nav-link">
+                Poznejte své město
+              </a>
+            </li>
+            <li>
+              <a href="#whyplay" className="nav-link">
+                Proč hrát naše hry
+              </a>
+            </li>
+            <li>
+              <a href="#buy" className="nav-link">
+                Koupit hru
+              </a>
+            </li>
+            <li>
+              <a href="#contact" className="nav-link">
+                Kontakt
+              </a>
+            </li>
+          </ul>
+        ) : null}
+      </div>
     </>
   );
 };
