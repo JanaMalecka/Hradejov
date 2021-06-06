5f86573add2e64529fc83e7056ae21204c191d69
Jana Malecka <jm.jmalecka@gmail.com>
2021-06-06 19:20:45 +0200

    metadata seo

--- webpack.config.js
+++ webpack.config.js
@@ -65,6 +65,7 @@ module.exports = {
         { from: 'src/assets', to: 'assets', noErrorOnMissing: true },
         { from: 'src/favicon.ico', to: '', noErrorOnMissing: true },
         { from: 'src/index.html', to: '' },
+        { from: 'src/sitemap.xml', to: '' },
       ],
     }),
   ],
