7f399729d4a749f8104cda96f5e40c7d39d9ddac
MarketHorak <mandle29@centrum.cz>
2021-06-12 16:23:55 +0200

    11

--- src/GamePage/index.jsx
+++ src/GamePage/index.jsx
@@ -1,16 +1,17 @@
-import './style.css';
-import React, { useState } from 'react';
-import obrazek1 from './img/godel.gif';
-import ruzova2 from './img/ruzova2.svg';
-import Hra from './components/Hra/hra';
-import { Link } from 'react-router-dom';
+import "./style.css";
+import React, { useState } from "react";
+import obrazek1 from "./img/godel.gif";
+import ruzova2 from "./img/ruzova2.svg";
+import Hra from "./components/Hra/hra";
+import { Link } from "react-router-dom";
 
 const GamePage = () => (
   <div className="game">
-   
     <div className="container">
-    <Link to="/" className="link">Hradějov</Link>
-    <header>
+      <Link to="/" className="link">
+        Hradějov
+      </Link>
+      <header>
         <img src={obrazek1} className="kurt" />
         {/*    <img src={obrazek} /> */}
         <h1 className="h1godel">UKÁZKOVÁ HRA UTRHČÍSLO</h1>
@@ -19,11 +20,14 @@ const GamePage = () => (
         <img src={ruzova2} className="bounce-in-top" />
 
         <Hra />
-     <div className="mistoSodpovedi">
-      <a href="">Nahoru</a> 
-      </div>
+        <div className="mistoSodpovedi">
+          <a href="">Nahoru</a>
+        </div>
       </main>
-      <footer id="gameFooter"></footer>
+      <footer id="gameFooter">
+        <button className="btnFooter">
+          <h1>2</h1></button>
+      </footer>
     </div>
   </div>
 );
